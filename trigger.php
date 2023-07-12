00854720a8ff1e7e2396b3dd6eac6f814254cace
Paige Julianne Sullivan <psulli26@jhu.edu>
2023-07-12 14:29:59 -0400

    removed error reporting line

--- trigger.php
+++ trigger.php
@@ -1,14 +1,10 @@
 <?php
-error_reporting(E_ALL);
-
 $data = \RestUtility::processRequest(true);
-
 $post = $data->getRequestVars();
 
 $dq = new DataQuality();
 
 $rule_info = $dq->getRule($post['rule_id'], NULL);
-
 // total hack to make it fix calculations
 // see Classes/DataQuality.php:1207
 if ($post['fixCalcs']) $_POST['action'] = 'fixCalcs';
